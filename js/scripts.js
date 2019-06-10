7a0e1827c9643e99ab12b0c0e89b10419f040ec4
sandeep872 <49954454+sandeep872@users.noreply.github.com>
2019-06-10 22:44:59 +0530

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,55 @@
+
+
+window.onload = function(){
+        
+    //Check File API support
+    if(window.File && window.FileList && window.FileReader)
+    {
+        var filesInput = document.getElementById("files");
+        
+        filesInput.addEventListener("change", function(event){
+            
+            var files = event.target.files; //FileList object
+            var output = document.getElementById("result");
+            
+            for(var i = 0; i< files.length; i++)
+            {
+                var file = files[i];
+                
+                //Only pics
+                if(!file.type.match('image'))
+                  continue;
+                
+                var picReader = new FileReader();
+                
+                picReader.addEventListener("load",function(event){
+                    
+                    var picFile = event.target;
+                    
+                    var div = document.createElement("div");
+                    
+                    div.innerHTML = "<img class='thumbnail' src='" + picFile.result + "'" +
+                            "title='" + picFile.name + "'/> <a href='javascript:void(0);' class='remove_pict'>X</a>";
+                    
+                    output.insertBefore(div,null);   
+                    div.children[1].addEventListener("click", function(event){
+                       div.parentNode.removeChild(div);
+                    });         
+                
+                });
+                
+                 //Read the image
+                picReader.readAsDataURL(file);
+            }                               
+           
+        });
+    }
+    else
+    {
+        console.log("Your browser does not support File API");
+    }
+}
+
 jQuery(document).ready(function() {
 jQuery('.menu-btn').on('click', function () {
     jQuery('#bs-navbar').toggleClass('slide-menu')
@@ -221,14 +273,17 @@ jQuery('.notification-menu-btn').click(function() {
     jQuery('.notification-menu').toggleClass("show-notification-menu");
 });
 
+
+});
+$(document).ready(function() {
 jQuery(function () {
   jQuery("#datepicker").datepicker({ 
         autoclose: true, 
         todayHighlight: true
   }).datepicker('update', new Date());
+});	
+	
 });
-});
-
 
 	//Phone format
 
@@ -741,5 +796,9 @@ jQuery("#myForm").validate({
   wrapper: "li",
   submitHandler: function() { alert("Submitted!") }
 });
+
+
+
+
 
 
